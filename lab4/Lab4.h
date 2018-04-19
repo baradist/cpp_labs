d10d4dacf5d63eacad0f02d7d818529b245d0936
Oleg Grigoryev <baradist@ya.ru>
2018-04-20 00:51:56 +0300

    lab4 task3a

--- lab4/Lab4.h
+++ lab4/Lab4.h
@@ -13,3 +13,16 @@ void Swap(T *pInt, T *pInt1);
 
 template<typename T>
 void Swap(T &val, T &val1);
+
+void task3();
+
+const size_t N = 2;
+const size_t M = 4;
+
+int Min(const int pInt[N][M]);
+
+template<typename T>
+int Min(T **ppT, size_t X, size_t Y);
+
+template<typename T>
+void printArray(T **arr, size_t X, size_t Y);
